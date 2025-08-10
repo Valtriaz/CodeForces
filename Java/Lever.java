890e9236a4ebefa83e6aaead3268956a075cbb3c
Valtriaz <valtriaz@gmail.com>
2025-08-10 21:25:57 +0530

    Completed Lever

---
+++ Java/Lever.java
@@ -0,0 +1,50 @@
+import java.io.*;
+
+public class Lever {
+    static class FastScanner {
+        private final InputStream in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+        FastScanner(InputStream is) { in = is; }
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
+        }
+        int nextInt() throws IOException {
+            int c;
+            while ((c = read()) <= ' ') if (c == -1) return Integer.MIN_VALUE;
+            int sign = 1;
+            if (c == '-') { sign = -1; c = read(); }
+            int val = 0;
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+            return val * sign;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner(System.in);
+        int t = fs.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for (int tc = 0; tc < t; tc++) {
+            int n = fs.nextInt();
+            int[] a = new int[n];
+            int[] b = new int[n];
+            for (int i = 0; i < n; i++) a[i] = fs.nextInt();
+            for (int i = 0; i < n; i++) b[i] = fs.nextInt();
+            int posSum = 0;
+            for (int i = 0; i < n; i++) {
+                if (a[i] > b[i]) posSum += (a[i] - b[i]);
+            }
+            int ans = posSum + 1;
+            sb.append(ans).append('\n');
+        }
+        System.out.printf("%s", sb);
+    }
+}
